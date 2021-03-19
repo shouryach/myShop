3255175819f0e456dcf01d33430fcb2c1df151b6
tanmay-pramanick <pramanicktanmay@gmail.com>
2021-03-19 18:18:58 +0530

    Side menu modification

--- src/app/all-components/header/profile-content/profile-content.component.ts
+++ src/app/all-components/header/profile-content/profile-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/all-Services/api.service';
 
 @Component({
   selector: 'app-profile-content',
@@ -9,10 +10,28 @@ export class ProfileContentComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
-  
+  user: any;
+  hide = false;
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.user = this.api.getCurrentUser();
+    this.user.subscribe(user => {
+      if (user) {
+        console.log("User s this ")
+        console.log(user);
+        this.hide = true;
+      }
+      else {
+        console.log("empty user", user);
+        this.hide = false;
+      }
+
+
+
+    });
+  }
 
 }
